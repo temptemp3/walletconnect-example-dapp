9a6b797014b3a0089852ed4fef53e944cf5ae5cd
Jason Paulos <jasonpaulos@users.noreply.github.com>
2021-05-18 10:20:20 -0700

    Deploy

---
+++ precache-manifest.02b914ac58b9b65ec4be3c9eb2c361d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a84a389af0f65136998",
+    "url": "/algorand-walletconnect-example-dapp/static/js/main.2a84a389.chunk.js"
+  },
+  {
+    "revision": "52da5e0548d2caffe8ce",
+    "url": "/algorand-walletconnect-example-dapp/static/js/1.52da5e05.chunk.js"
+  },
+  {
+    "revision": "38ff1b95cb7475f6c192",
+    "url": "/algorand-walletconnect-example-dapp/static/js/runtime~main.38ff1b95.js"
+  },
+  {
+    "revision": "e1cb8d21bffbcc2a8c7bfcf95bad12ff",
+    "url": "/algorand-walletconnect-example-dapp/static/media/walletconnect-banner.e1cb8d21.png"
+  },
+  {
+    "revision": "6c6f52b09af2c84c6b4f2f7ce488b645",
+    "url": "/algorand-walletconnect-example-dapp/static/media/algo.6c6f52b0.svg"
+  },
+  {
+    "revision": "4670afd00554593ed2074ab4ea7db412",
+    "url": "/algorand-walletconnect-example-dapp/index.html"
+  }
+];
